86bc68221245113ac35557e71b21690f0340eb13
Pratik Pote <pratik.pote89@gmail.com>
2026-02-09 01:35:01 +0530

    now opens in new browser

--- launch.bat
+++ launch.bat
@@ -8,8 +8,8 @@ echo.
 
 cd /d "%~dp0"
 
-:: Wait 2 seconds then open browser
-start "" cmd /c "timeout /t 2 /nobreak >nul && start http://localhost:5000"
+:: Wait 2 seconds then open in a NEW browser window
+start "" cmd /c "timeout /t 2 /nobreak >nul && python -m webbrowser -n http://localhost:5000"
 
 :: Start the Flask app
 python app.py
